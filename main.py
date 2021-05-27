37b3b9a38e282c203a7b8ada9e05207d98cb126e
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2021-05-26 21:14:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,23 @@ por_animo = {}
 
 
 def porArtista(ses):
-    canciones = ses.run("MATCH (a:Artista) return a")
-    lista = [nodo["a"] for nodo in canciones]
-    for n in lista:
-        print(n["nombre"])
+
+    #Se ingresa el artista que desea buscar las canciones
+    artista = input("\nIngrese el nombre del artista que quiere escuchar -> ")
+    #Se busca el mach de que canciones canta el artista para guardarlas
+    canciones = ses.run("MATCH (a:Artista{nombre:'%s'})-[:Canta]->(c:Cancion) return c"% artista.lower())
+
+    #Se agregan a una lista
+    lista = [nodo["c"] for nodo in canciones]
+    
+    #Se van imprimiento las canciones encontradas que hicieron mach
+    if len(lista) == 0:
+        print("\nEl artista que ha ingresado no existe.")
+    else:
+        print("\nLas canciones que te recomendamos de "+artista+" son:")
+        for i in range(0,len(lista)):
+            print("\t-> "+lista[i]["nombre"])
+
     return canciones
 
 
